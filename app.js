3792301f461a7e8085f6bf398adb73aa54d65735
Dan Collins <dan.collins.dev@gmail.com>
2026-09-01 19:22:42 -0400

    created an example .env, moved .http files into their own folder, and created app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+console.log("==========================");
+console.log("=         ST CLI         =");
+console.log("==========================");
+
+console.log("\nDo the thing")
